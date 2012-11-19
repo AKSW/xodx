6dc1e363d0a2dbdc1d975224abad8da93b432841
Natanael Arndt <arndtn@gmail.com>
2012-11-19 21:26:54 +0100

    update Notification and add NotificationFactory

--- classes/Xodx/Notification.php
+++ classes/Xodx/Notification.php
@@ -1,5 +1,47 @@
 <?php
 
+/**
+ * This class represents notifications which are stored for a user to let him know, that some
+ * action happend (e.g. new message, new friend request).
+ */
 class Xodx_Notification
 {
+    /**
+     * The uri of the notification represented by this object
+     */
+    private $_uri;
+
+    /**
+     * The uri of the user to whome this message may concern
+     */
+    private $_userUri;
+
+    /**
+     * A resource related to this notification, e.g. an Activity
+     */
+    private $_seeAlsoResource;
+
+    /**
+     * setter method for this objects uri
+     */
+    public function setUri ($notificationUri)
+    {
+        $this->_uri = $notificationUri;
+    }
+
+    /**
+     * setter method for this objects uri
+     */
+    public function setUserUri ($userUri)
+    {
+        $this->_userUri = $userUri;
+    }
+
+    /**
+     * setter method for this objects uri
+     */
+    public function setSeeAlsoUri ($seeAlsoResource)
+    {
+        $this->_seeAlsoResource = $seeAlsoResource;
+    }
 }
---
+++ classes/Xodx/NotificationFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This factory creates new notification objects
+ */
+class Xodx_NotificationFactory
+{
+    /**
+     * The reference to the currently valid application instace
+     */
+    private $_app;
+
+    /**
+     * The construct method for this factory
+     * @param $app the currently valid application instance
+     */
+    public __construct ($app) {
+        $this->_app = $app;
+    }
+
+    /**
+     * Get a new Xodx_Notification instance from the model
+     */
+    public function fromModel ($notificationUri)
+    {
+        // TODO get notification from model
+
+        $notification = new Xodx_Notification();
+        $notification->setUri($notificationUri);
+
+        // TODO set properties
+    }
+}
