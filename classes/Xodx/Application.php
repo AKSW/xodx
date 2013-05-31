0adb25fc830351aa4eb2a99d26baae16dd03c663
Natanael Arndt <arndtn@gmail.com>
2013-05-31 13:00:20 +0200

    Improve error reporting. Fix #29.

--- classes/Xodx/Application.php
+++ classes/Xodx/Application.php
@@ -10,51 +10,56 @@ class Xodx_Application extends Saft_Application
     public function run() {
         parent::run();
 
-        // TODO: parse request uri to determine correct controller
+        try {
+            $bootstrap = $this->getBootstrap();
 
-        $bootstrap = $this->getBootstrap();
+            $appController = $this->getController('Xodx_ApplicationController');
+            $appController->authenticate();
 
-        $appController = $this->getController('Xodx_ApplicationController');
-        $appController->authenticate();
+            /**
+             * Prepare Template
+             */
+            $this->_layout->setLayout('templates/layout.phtml');
+            $this->_layout->addMenu('templates/menu.phtml');
 
-        /**
-         * Prepare Template
-         */
-        $this->_layout->setLayout('templates/layout.phtml');
-        $this->_layout->addMenu('templates/menu.phtml');
+            $request = $bootstrap->getResource('request');
 
-        $request = $bootstrap->getResource('request');
+            if ($request->hasValue('c')) {
+                $requestController = ucfirst(strtolower($request->getValue('c'))); // 'get'
+            } else {
+                $requestController = ucfirst(strtolower('index'));
+            }
 
-        if ($request->hasValue('c')) {
-            $requestController = ucfirst(strtolower($request->getValue('c'))); // 'get'
-        } else {
-            $requestController = ucfirst(strtolower('index'));
+            if ($request->hasValue('a')) {
+                $requestAction = strtolower($request->getValue('a'));
+
+            } else {
+                $requestAction = 'index';
+            }
+
+            $controllerName = $this->_appNamespace . $requestController . 'Controller';
+
+            $actionName = $requestAction . 'Action';
+            $controller = $this->getController($controllerName);
+            $this->_layout = $controller->$actionName($this->_layout);
+
+            $userController = $this->getController('Xodx_UserController');
+            $user = $userController->getUser();
+
+            $this->_layout->username = $user->getName();
+            $this->_layout->notifications = $userController->getNotifications($user->getUri());
+
+            $config = $bootstrap->getResource('config');
+            if (isset($config['debug']) && $config['debug'] == false) {
+                $this->_layout->disableDebug();
+            }
+        } catch (Exception $e) {
+            $this->_layout->setLayout('templates/errorlayout.phtml');
+            $this->_layout->errorType = 'Application Error';
+            $this->_layout->exception = $e;
+            $this->_layout->back = $this->getBaseUri();
+            $this->_layout->addContent('templates/error.phtml');
         }
-
-        if ($request->hasValue('a')) {
-            $requestAction = strtolower($request->getValue('a'));
-
-        } else {
-            $requestAction = 'index';
-        }
-
-        $controllerName = $this->_appNamespace . $requestController . 'Controller';
-
-        $actionName = $requestAction . 'Action';
-        $controller = $this->getController($controllerName);
-        $this->_layout = $controller->$actionName($this->_layout);
-
-        $userController = $this->getController('Xodx_UserController');
-        $user = $userController->getUser();
-
-        $this->_layout->username = $user->getName();
-        $this->_layout->notifications = $userController->getNotifications($user->getUri());
-
-        $config = $bootstrap->getResource('config');
-        if (isset($config['debug']) && $config['debug'] == false) {
-            $this->_layout->disableDebug();
-        }
-
         $this->_layout->render();
     }
 }
