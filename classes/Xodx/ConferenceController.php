803ba83e875530be3145c8933b2e0cab8ee53c11
Rico Feist <rf@navis-soft.de>
2013-10-21 14:08:49 +0200

    Removed "edit" from ConferenceController.php

--- classes/Xodx/ConferenceController.php
+++ classes/Xodx/ConferenceController.php
@@ -23,51 +23,6 @@ class Xodx_ConferenceController extends Xodx_ResourceController
         var_dump($configHelper -> loadPropertiesSingle("conference"));
     }
 
-    public function editAction($template)
-    {
-        $bootstrap = $this->_app->getBootstrap();
-        $model = $bootstrap->getResource('model');
-        $configHelper = new Xodx_ConfigHelper($this->_app);
-        $allowedSinglePrefixes = $configHelper->loadPropertiesSingle("conference");
-        $request = $bootstrap->getResource('request');
-
-        if (count ($_POST) == 0)
-        {
-            $objectId = $request->getValue('id', 'get');
-            //Show editor with data from database
-            $applicationController = $this->_app->getController('Xodx_ApplicationController');
-            $userId = $applicationController->getUser();
-            //$userUri = $this->_app->getBaseUri() . '?c=person&id=' . $userId;
-            $stringArray = explode("id=", $userUri);
-            $name = $stringArray[1];
-            if (!is_null($objectId))
-            {
-                $eventUri = $objectId;
-                $typeUri = $configHelper -> getEditorClass("conference");
-
-                $query = "PREFIX ns2: <http://symbolicdata.org/Data/Model#> SELECT ?p ?o WHERE { <" . $eventUri . "> a <" . $typeUri . "> . <" . $eventUri . "> ?p ?o }";
-                //$query = "PREFIX  ical: <http://www.w3.org/2002/12/cal/ical#> SELECT ?event ?p ?o WHERE { ?event a ical:Event . ?event ?p ?o }";
-
-                $profiles = $model->sparqlQuery( $query);
-                $template->allowedSinglePrefixes = $allowedSinglePrefixes;
-                $template->profile = $profiles;
-                $template->addContent('templates/edit.phtml');
-                //echo ("FOOO");
-                return $template;
-            }
-            else
-            {
-                //this will create a new conference
-                return $template;
-            }
-        }
-        else
-        {
-            //Currently do nothing.
-            echo ("Nope.");
-        }
-    }
-
     public function listAction($template)
     {
         $model = $this->_app->getBootstrap()->getResource('Model');
